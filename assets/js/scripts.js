a5a9575a09e68cad21ff32acd7b7d35b63705352
Raja Ziafat <rajaziafat3258@gmail.com>
2021-07-08 00:10:17 +0500

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+
+	// banner slider
+	$('.partner-carousel').owlCarousel({
+	    loop:true,
+	    margin:30,
+	    autoplay:true,
+	    nav:false,
+	    autoplayTimeout:3000,	    
+	    responsiveClass:true,
+	    responsive:{
+	        0:{
+	            items:2,
+	        },
+	        414:{
+	        	items:3,
+	        },
+	        576:{
+	        	items:4,
+	        },
+	        922:{
+	        	items:5,
+	        }
+
+	    }
+	});
+
+
+	// mobile humbar
+	$('.all-p-humber').click(function(){
+		$(this).toggleClass('open');
+	});	
+
+	// Start Humber Main Menu Icon Js
+	$('.all-p-humber').click(function(){
+		$('.main_menu').slideToggle(300);
+	});
+})
